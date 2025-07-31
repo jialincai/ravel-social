a00009cee82f17cfc20e6ac19085f191f4e60bd3
Jialin Cai <62089747+jialincai@users.noreply.github.com>
2025-07-30 22:19:09 -0400

    5-feature-create-initial-tabs (#6)
    * add prettier linting
    
    * add tabs
    
    ---------
    
    Co-authored-by: Jialin Cai <jiali@DESKTOP-5H8CFNL>

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list.bullet" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="envelope.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
