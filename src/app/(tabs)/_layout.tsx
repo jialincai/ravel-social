ec6f6639f283d21c8b51ea8693a5d59c6f4b1cc4
Jialin Cai <jiali@DESKTOP-5H8CFNL>
2025-07-31 14:40:56 -0400

    add theme components

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,11 +3,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
